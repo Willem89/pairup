c197bfbb3d2c47c54b296c3a6059a285375cce22
WILLEM SINNIGE <W.SINNIGE@GMAIL.COM>
2017-09-18 13:56:53 +0200

    Changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Student.destroy_all
+
+student1 = Student.create!(first_name: "Marco", last_name: "Polo",)
+student2 = Student.create!(first_name: "Winston", last_name: "Churchil",)
+student3 = Student.create!(first_name: "Karel", last_name: "de Grote",)
+student4 = Student.create!(first_name: "Salah", last_name: "Saladin",)
+student5 = Student.create!(first_name: "Julius", last_name: "Ceasar",)
+student6 = Student.create!(first_name: "Marcus", last_name: "Aurelius",)
+student7 = Student.create!(first_name: "Lara", last_name: "Croft",)
+student8 = Student.create!(first_name: "Jeanne", last_name: "d'Arc",)
+student9 = Student.create!(first_name: "Mother", last_name: "Theresa",)
+student10 = Student.create!(first_name: "Charles", last_name: "de Gaulle",)
+student11 = Student.create!(first_name: "Dzjengis", last_name: "Khan",)
+student12 = Student.create!(first_name: "Ellie", last_name: "Tlou",)
